b0cdfed06b9a608e7bfb79b8e0fa96822ecb360b
Ritam  Sengupta <ritam_s@pursuitsoftware.biz>
2025-06-26 11:21:36 +0530

    Initial commit: Flask slot-checker API

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+available_slots = {
+    "2025-06-27": ["9", "10", "11", "14"],
+    "2025-06-28": ["10", "11", "15"]
+}
+
+@app.route('/check_slots', methods=['POST'])
+def check_slots():
+    data = request.get_json()
+    date = data.get("date")
+    time = data.get("time")
+
+    if not date or not time:
+        return jsonify({"error": "Missing date or time"}), 400
+
+    available = time in available_slots.get(date, [])
+    return jsonify({"available": available}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
